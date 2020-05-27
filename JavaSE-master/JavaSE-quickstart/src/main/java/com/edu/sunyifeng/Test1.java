9cc5075131a0edc119449896d9132fec13dff071
Code-Sun <38420811+Code-Sun@users.noreply.github.com>
2020-05-28 00:09:18 +0800

    Update Test1.java
    11

--- JavaSE-master/JavaSE-quickstart/src/main/java/com/edu/sunyifeng/Test1.java
+++ JavaSE-master/JavaSE-quickstart/src/main/java/com/edu/sunyifeng/Test1.java
@@ -8,7 +8,7 @@ package com.edu.sunyifeng;
 public class Test1{
 
     public static void main(String[] args) {
- System.out.println(557*32);
+ System.out.println(111);
 
         System.out.println();
 
